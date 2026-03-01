2f574ff6ca6d8da7009afab366d63b6dd2a690ef
SWAGAT TEEJORAM <swagat7502@gmail.com>
2026-03-01 21:10:20 +0530

    Done with Recursion

--- src/3.Recursion/Exercises.cpp
+++ src/3.Recursion/Exercises.cpp
@@ -62,6 +62,61 @@ int FibonocciMemoization(int N)
     }
 }
 
+// nCr Using Recursion
+// Combination Formula =>
+// nCr = n!/ r!(n-r)!
+
+// Normal Function
+int nCrNormal(int n, int r)
+{
+    int t1, t2, t3;
+    t1 = factorial(n);
+    t2 = factorial(r);
+    t3 = factorial(n-r);
+    return t1/t2*t3;
+}
+
+// Recursive Function using Pascal Triangle
+/*
+Pascal Identity:
+-> nCr = (n-1)C(r-1) + (n-1)Cr
+
+Meaning:
+-> Each value in Pascal's Triangle is the sum of two values directly above it:
+
+            1     -> COLUMN 1
+           1 1    -> COLUMN 2
+          1 2 1   -> COLUMN 3
+            ↑
+          1 + 1
+so -> 3C1 = 2C0 + 2C1
+
+Here when
+    nC0 = 1      (choosing nothing)
+    nCn = 1      (choosing everything)
+*/
+
+int nCrRecursive(int n, int r)
+{
+    if(r==0 || n==r)
+       return 1;
+    else
+       return nCrRecursive(n-1, r-1) +  nCrRecursive(n-1, r);
+}
+
+
+// Tower of Hanoi
+// Refer the local notes or Abdul Bari's Video for reference
+void TOH(int N, int A ,int B, int C)
+{
+    if (N>0)
+    {
+        TOH(N-1, A, C, B); // Move N-1 Disks from A -> B using C
+        std::cout << "(" << A << ", " << C << ")" << std::endl; // Move Disk A -> C
+        TOH(N-1, B, A, C); // Move N-1 Disks from B -> C using A
+    }
+}
+
 int main()
 {
     // int n = 5;
@@ -71,8 +126,14 @@ int main()
 
     // std::cout << "Power using Recursion: "<< powerUsingRecursion(2, 5) << std::endl;
 
-    int n = 10;
-    std::cout << "Brute Force: " << FibonacciBruteForce(n) << std::endl;
-    std::cout << "Memoization: " << FibonocciMemoization(n) << std::endl;
+    // int n = 10;
+    // std::cout << "Brute Force: " << FibonacciBruteForce(n) << std::endl;
+    // std::cout << "Memoization: " << FibonocciMemoization(n) << std::endl;
+    
+    // int N=5;
+    // std::cout << "Normal Function: " << nCrNormal(N, 4) << std::endl;
+    // std::cout << "Recursive Function:" << nCrRecursive(N, 4);
+    
+    TOH(3, 1, 2, 3);
     return 0;
 }
